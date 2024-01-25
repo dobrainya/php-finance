04cddd4759e980282f7f76e150530cd9a23d9a7a
dobrainya <silnbrains@gmail.com>
2024-01-25 11:54:09 -0500

    @7 create functional tests

---
+++ tests/Controller/AmountControllerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AmountControllerTest extends WebTestCase
+{
+    public function testGetIncomes(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/api/amount/incomes');
+        $responseText = $client->getResponse()->getContent();
+
+        $this->assertResponseIsSuccessful();
+        $this->assertJsonStringEqualsJsonFile(
+            __DIR__ . '/Responses/AmountControllerTest_testGetIncomes.json',
+            $responseText
+        );
+    }
+
+    public function testGetExpenses(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/api/amount/expenses');
+        $responseText = $client->getResponse()->getContent();
+
+        $this->assertResponseIsSuccessful();
+        $this->assertJsonStringEqualsJsonFile(
+            __DIR__ . '/Responses/AmountControllerTest_testGetExpenses.json',
+            $responseText
+        );
+    }
+}
